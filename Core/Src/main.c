a2e0f479e007c6e2c81acd3c76e7344e3aef104e
alancristian2 <50837767+alancristian2@users.noreply.github.com>
2025-07-25 18:39:03 +0200

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,7 +1,9 @@
+// main.c
 #include "main.h"
 #include "config.h"
 #include "print.h"
 #include "motor.h"
+#include "motor_param.h"
 #include "button_processing.h"
 #include "M365_Dashboard.h"
 
@@ -17,6 +19,41 @@ M365State_t M365State;
 MotorStatePublic_t MSPublic;
 
 volatile uint32_t systick_cnt = 0;
+
+// Tabla de estados para sensores Hall (3 sensores)
+const uint8_t hall_table[8] = {
+    0,  // 000 - no válido
+    5,  // 001
+    3,  // 010
+    6,  // 011
+    1,  // 100
+    4,  // 101
+    2,  // 110
+    0   // 111 - no válido
+};
+
+// Parámetros motor Pro 4 (40 imanes, 20 pares de polos)
+MotorParams_t motorParams = {
+    .pole_pairs = 20,
+    .bemf_constant = 42.0f,
+    .max_speed_rpm = 3000,
+
+    .disable_auto_detect = 1, // desactivada autodetección
+
+    .hall_offset_1 = 60,
+    .hall_offset_2 = 150,
+    .hall_invert_c = 1,
+
+    .phase_current_max_eco = 10000,
+    .phase_current_max_normal = 15000,
+    .phase_current_max_sport = 25000,
+    .regen_max_current = 10000,
+    .field_weakening_current_max = 0,
+
+    .phase_resistance = 0.135f,
+    .phase_inductance = 0.00018f,
+    .flux_linkage = 0.023f
+};
 
 // every 1ms
 void UserSysTickHandler(void) {
@@ -40,17 +77,14 @@ static void DMA_Init(void) {
   __HAL_RCC_DMA1_CLK_ENABLE();
 
   // DMA channel 3: used for USART3_RX
-  /* DMA1_Channel3_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel3_IRQn, 3, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel3_IRQn);
 
   // DMA channel 4: used for USART1_TX
-  /* DMA1_Channel4_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel4_IRQn, 3, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel4_IRQn);
 
   // DMA channel 5: used for USART1_RX
-  /* DMA1_Channel5_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel5_IRQn, 3, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel5_IRQn);
 }
@@ -64,8 +98,6 @@ void SystemClock_Config(void) {
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
   RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
-  /**Initializes the CPU, AHB and APB busses clocks
-   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = 16;
@@ -76,8 +108,6 @@ void SystemClock_Config(void) {
     _Error_Handler(__FILE__, __LINE__);
   }
 
-  /**Initializes the CPU, AHB and APB busses clocks
-   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
       | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -95,15 +125,8 @@ void SystemClock_Config(void) {
     _Error_Handler(__FILE__, __LINE__);
   }
 
-  /**Configure the Systick interrupt time
-   */
   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
-
-  /**Configure the Systick
-   */
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-
-  /* SysTick_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(SysTick_IRQn, 2, 0);
 }
 
@@ -113,13 +136,12 @@ void SystemClock_Config(void) {
  * @retval None
  */
 static void USART1_UART_Init(void) {
-
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;           // Full duplex TX/RX
+  huart1.Init.Mode = UART_MODE_TX_RX;
   huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
   if (HAL_UART_Init(&huart1) != HAL_OK) {
@@ -133,11 +155,8 @@ static void USART1_UART_Init(void) {
  * @retval None
  */
 static void USART3_UART_Init(void) {
-
   huart3.Instance = USART3;
-
   huart3.Init.BaudRate = 115200;
-
   huart3.Init.WordLength = UART_WORDLENGTH_8B;
   huart3.Init.StopBits = UART_STOPBITS_1;
   huart3.Init.Parity = UART_PARITY_NONE;
@@ -157,54 +176,48 @@ static void USART3_UART_Init(void) {
 static void GPIO_Init(void) {
   GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
-  /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
-  /* Configure GPIO pin : PWR_BTN_Pin */
+  // PWR_BTN (input)
   GPIO_InitStruct.Pin = PWR_BTN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(PWR_BTN_GPIO_Port, &GPIO_InitStruct);
 
-  /* Configure GPIO pin Output Level */
+  // TPS_ENA (output set)
   HAL_GPIO_WritePin(TPS_ENA_GPIO_Port, TPS_ENA_Pin, GPIO_PIN_SET);
-
-  /* Configure GPIO pin : TPS_ENA_Pin */
   GPIO_InitStruct.Pin = TPS_ENA_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(TPS_ENA_GPIO_Port, &GPIO_InitStruct);
 
-  /* Configure GPIO pin Output Level */
+  // LED (output reset)
   HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-
-  /* Configure GPIO pin : LED_Pin */
   GPIO_InitStruct.Pin = LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
-  /* Configure GPIO pins for UART1 Tx (PB6) and Rx (PB7) */
+  // UART1 Tx (PB6) - AF push-pull
   GPIO_InitStruct.Pin = GPIO_PIN_6;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;           // Alternate function push-pull for TX
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+  // UART1 Rx (PB7) - input
   GPIO_InitStruct.Pin = GPIO_PIN_7;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;           // Input for RX
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /* Configure GPIO pin Output Level */
+  // BrakeLight (PA15) Open Drain output
   HAL_GPIO_WritePin(BrakeLight_GPIO_Port, BrakeLight_Pin, GPIO_PIN_RESET);
-
-  /* Configure GPIO pin : BrakeLight_Pin */
   GPIO_InitStruct.Pin = BrakeLight_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -213,17 +226,13 @@ static void GPIO_Init(void) {
 }
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *UartHandle) {
-  // En modo full duplex no necesitas habilitar receptor manualmente
+  // En modo full duplex no hace falta habilitar receptor manualmente
 }
 
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *UartHandle) {
-  // Puedes agregar manejo de error si quieres
+  // Puedes añadir manejo de error
 }
 
-/**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
 void Error_Handler(void) {
   __disable_irq();
   while (1) {
@@ -239,9 +248,9 @@ void _Error_Handler(char *file, int line) {
 
 #ifdef  USE_FULL_ASSERT
 void assert_failed(uint8_t *file, uint32_t line) {
-  // Puedes implementar reporte de errores
+  // Implementa reporte de errores si quieres
 }
-#endif /* USE_FULL_ASSERT */
+#endif
 
 int main(void) {
   HAL_Init();
@@ -255,13 +264,16 @@ int main(void) {
   USART1_UART_Init();
   USART3_UART_Init();
 
-  MSPublic.brake_active = true; // force motor stop until receive real value from brake
-  MSPublic.i_q_setpoint_target = 0; // start at 0 until throttle value is read
-  MSPublic.speed = 128000;
+  MSPublic.brake_active = true; // fuerza a freno activo hasta recibir datos reales
+  MSPublic.i_q_setpoint_target = 0; // empezar con acelerador 0
+  MSPublic.speed = 128000; // valor inicial (escala interna)
   MSPublic.speed_limit = SPEEDLIMIT_NORMAL;
   MSPublic.phase_current_limit = PH_CURRENT_MAX_NORMAL;
   MSPublic.field_weakening_current_max = FIELD_WEAKNING_CURRENT_MAX;
   MSPublic.battery_voltage_min = BATTERYVOLTAGE_MIN;
+
+  // Cargar parámetros motor desde motorParams
+  motor_load_default_params(&MSPublic);
 
   motor_init(&MSPublic);
 
