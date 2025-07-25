65e3a2ef384482151124822c8af9cb9dcb137d98
alancristian2 <50837767+alancristian2@users.noreply.github.com>
2025-07-25 20:08:24 +0200

    Update main.h

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -1,4 +1,27 @@
-#pragma once
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -6,44 +29,105 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f1xx_hal.h"
+#include "product.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <string.h>
-#include <stdlib.h>
-#include <arm_math.h>
-#include <stdbool.h>
-#include "utils.h"
-#include "motor.h"
+#include "product.h"
+/* USER CODE END Includes */
 
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef huart3;
+extern DMA_HandleTypeDef hdma_usart1_rx;
+extern DMA_HandleTypeDef hdma_usart1_tx;
+extern DMA_HandleTypeDef hdma_usart3_tx;
+extern DMA_HandleTypeDef hdma_usart3_rx;
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
-extern void UserSysTickHandler(void);
+/* USER CODE BEGIN EFP */
 
-typedef struct {
-  q31_t battery_voltage;
-  int16_t phase_current_limit;
-  int16_t regen_max_current;
-	int8_t temperature;
-	int8_t mode;
-	bool light;
-	bool beep;
-  int32_t i_q_setpoint_target;
-  uint32_t speed;
-	bool brake_active;
-  uint32_t shutdown;
-  int8_t speed_limit;
-  int8_t error_state;
-} M365State_t;
+/* USER CODE END EFP */
 
-enum modes {
-  eco = 2,
-  normal = 0,
-  sport = 4
-};
+/* Private defines -----------------------------------------------------------*/
+#define PWR_BTN_Pin GPIO_PIN_14
+#define PWR_BTN_GPIO_Port GPIOC
+#define TPS_ENA_Pin GPIO_PIN_15
+#define TPS_ENA_GPIO_Port GPIOC
+#define LED_Pin GPIO_PIN_1
+#define LED_GPIO_Port GPIOD
+#define M1_TEMPERATURE_Pin GPIO_PIN_0
+#define M1_TEMPERATURE_GPIO_Port GPIOA
+#define M1_BUS_VOLTAGE_Pin GPIO_PIN_2
+#define M1_BUS_VOLTAGE_GPIO_Port GPIOA
+#define M1_CURR_AMPL_U_Pin GPIO_PIN_3
+#define M1_CURR_AMPL_U_GPIO_Port GPIOA
+#define M1_CURR_AMPL_V_Pin GPIO_PIN_4
+#define M1_CURR_AMPL_V_GPIO_Port GPIOA
+#define M1_CURR_AMPL_W_Pin GPIO_PIN_5
+#define M1_CURR_AMPL_W_GPIO_Port GPIOA
+#define PHASE_A_V_Pin GPIO_PIN_6
+#define PHASE_A_V_GPIO_Port GPIOA
+#define PHASE_B_V_Pin GPIO_PIN_7
+#define PHASE_B_V_GPIO_Port GPIOA
+#define M1_HALL_H3_Pin GPIO_PIN_0
+#define M1_HALL_H3_GPIO_Port GPIOB
+#define PHASE_C_V_Pin GPIO_PIN_1
+#define PHASE_C_V_GPIO_Port GPIOB
+#define UART_TX_Pin GPIO_PIN_10
+#define UART_TX_GPIO_Port GPIOB
+#define UART_RX_Pin GPIO_PIN_11
+#define UART_RX_GPIO_Port GPIOB
+#define M1_PWM_UL_Pin GPIO_PIN_13
+#define M1_PWM_UL_GPIO_Port GPIOB
+#define M1_PWM_VL_Pin GPIO_PIN_14
+#define M1_PWM_VL_GPIO_Port GPIOB
+#define M1_PWM_WL_Pin GPIO_PIN_15
+#define M1_PWM_WL_GPIO_Port GPIOB
+#define M1_PWM_UH_Pin GPIO_PIN_8
+#define M1_PWM_UH_GPIO_Port GPIOA
+#define M1_PWM_VH_Pin GPIO_PIN_9
+#define M1_PWM_VH_GPIO_Port GPIOA
+#define M1_PWM_WH_Pin GPIO_PIN_10
+#define M1_PWM_WH_GPIO_Port GPIOA
+#define BRAKE_LIGHT_Pin GPIO_PIN_15
+#define BRAKE_LIGHT_GPIO_Port GPIOA
+#define M1_HALL_H1_Pin GPIO_PIN_4
+#define M1_HALL_H1_GPIO_Port GPIOB
+#define M1_HALL_H2_Pin GPIO_PIN_5
+#define M1_HALL_H2_GPIO_Port GPIOB
 
-void _Error_Handler(char*, int);
+/* USER CODE BEGIN Private defines */
+/* Alias para compatibilidad con stm32f1xx_hal_msp.c */
+#define Phase_Current_1_Pin M1_CURR_AMPL_U_Pin
+#define Phase_Current_2_Pin M1_CURR_AMPL_V_Pin
+#define Phase_Current_3_Pin M1_CURR_AMPL_W_Pin
+
+#define HALL_1_Pin M1_HALL_H1_Pin
+#define HALL_2_Pin M1_HALL_H2_Pin
+#define HALL_3_Pin M1_HALL_H3_Pin
+/* USER CODE END Private defines */
 
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
